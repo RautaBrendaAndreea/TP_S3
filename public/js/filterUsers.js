848975a6b621c1b40b594c06c3d2f7b7052b89d3
Melissa França Jorge <melissafranca3@gmail.com>
2024-07-03 10:11:04 +0200

    fix décalage des boutons quand filtre appliqué

--- public/js/filterUsers.js
+++ public/js/filterUsers.js
@@ -1,45 +1,43 @@
-const searchNameInput = document.getElementById('searchName');
-const searchLocationInput = document.getElementById('searchLocation');
-const searchCategorySelect = document.getElementById('searchCategory');
-const cards = document.querySelectorAll('.card');
+const searchNameInput = document.getElementById("searchName");
+const searchLocationInput = document.getElementById("searchLocation");
+const searchCategorySelect = document.getElementById("searchCategory");
+const cards = document.querySelectorAll(".card");
 
-searchNameInput.addEventListener('input', () => {
+searchNameInput.addEventListener("input", () => {
   const searchName = searchNameInput.value.trim().toLowerCase();
-  filterUsers(searchName, 'name');
+  filterUsers(searchName, "name");
 });
 
-searchLocationInput.addEventListener('input', () => {
+searchLocationInput.addEventListener("input", () => {
   const searchLocation = searchLocationInput.value.trim().toLowerCase();
-  filterUsers(searchLocation, 'location');
+  filterUsers(searchLocation, "location");
 });
 
-searchCategorySelect.addEventListener('change', () => {
+searchCategorySelect.addEventListener("change", () => {
   const searchCategory = searchCategorySelect.value;
-  filterUsers(searchCategory, 'category');
+  filterUsers(searchCategory, "category");
 });
 
 function filterUsers(searchTerm, type) {
-  
-    cards.forEach(card => {
-      const name = card.getAttribute('data-name');
-      const location = card.getAttribute('data-location');
-      const category = card.getAttribute('data-category');
+  cards.forEach((card) => {
+    const name = card.getAttribute("data-name");
+    const location = card.getAttribute("data-location");
+    const category = card.getAttribute("data-category");
 
-      let shouldDisplay = true;
-  
-      if (type === 'name' && name) {
-        shouldDisplay = name.toLowerCase().includes(searchTerm);
-      } else if (type === 'location' && location) {
-        shouldDisplay = location.toLowerCase().includes(searchTerm);
-      } else if (type === 'category' && category) {
-        shouldDisplay = (searchTerm === '' || category === searchTerm);
-      }
-  
-      if (shouldDisplay) {
-        card.style.display = 'block';
-      } else {
-        card.style.display = 'none';
-      }
-    });
-  }
-  
+    let shouldDisplay = true;
+
+    if (type === "name" && name) {
+      shouldDisplay = name.toLowerCase().includes(searchTerm);
+    } else if (type === "location" && location) {
+      shouldDisplay = location.toLowerCase().includes(searchTerm);
+    } else if (type === "category" && category) {
+      shouldDisplay = searchTerm === "" || category === searchTerm;
+    }
+
+    if (shouldDisplay) {
+      card.style.display = "flex";
+    } else {
+      card.style.display = "none";
+    }
+  });
+}
