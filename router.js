b0f6be522957d5dcee38e8b6a89da8e335c1c7b9
Melissa França Jorge <melissafranca3@gmail.com>
2024-07-04 10:36:43 +0200

    protection route home

--- router.js
+++ router.js
@@ -6,24 +6,21 @@ import {
   handleLogin,
   handleLogout,
 } from "./src/controllers/authController.js";
-import { 
-  showUser, 
-  showAllUsers,  
-  updateUser, 
-  showEditForm 
+import {
+  showUser,
+  showAllUsers,
+  updateUser,
+  showEditForm,
 } from "./src/controllers/userController.js";
 import {
-  deleteUser, 
-  showAddUserForm, 
-  addNewUser, 
-  updateAdminUser, 
-  showAdminEditForm
-} from './src/controllers/adminController.js'
+  deleteUser,
+  showAddUserForm,
+  addNewUser,
+  updateAdminUser,
+  showAdminEditForm,
+} from "./src/controllers/adminController.js";
 
 const router = express.Router();
-
-router.use(authMiddleware);
-
 
 // Login/Logout
 router.get("/", (req, res) => res.redirect("/login"));
@@ -31,29 +28,28 @@ router.get("/login", showLoginPage);
 router.post("/login", handleLogin);
 router.get("/logout", handleLogout);
 
+// Application du middleware d'authentification aux route suivantes
+router.use(authMiddleware);
 
 // Route pour gérer l'affichage de la page d'accueil et des users
 router.get("/home", showUser);
-router.get('/listing', showAllUsers);
-
+router.get("/listing", showAllUsers);
 
 // Route GET et POST pour mettre à jour les informations du profil
-router.get('/edit', showEditForm);
-router.post('/edit/:userId', updateUser);
-
+router.get("/edit", showEditForm);
+router.post("/edit/:userId", updateUser);
 
 // Route pour la suppression d'un utilisateur
 router.delete("/delete/:userId", deleteUser);
 
 // Routes d'administration
-router.get('/admin/add',adminMiddleware, showAddUserForm);
-router.post('/admin/add',adminMiddleware, addNewUser); 
-router.get('/admin/edit/:userId', adminMiddleware, showAdminEditForm);
-router.post('/admin/edit/:userId', adminMiddleware, updateAdminUser);
-
+router.get("/admin/add", adminMiddleware, showAddUserForm);
+router.post("/admin/add", adminMiddleware, addNewUser);
+router.get("/admin/edit/:userId", adminMiddleware, showAdminEditForm);
+router.post("/admin/edit/:userId", adminMiddleware, updateAdminUser);
 
 router.use((req, res, next) => {
-  res.status(404).send('Page not found');
+  res.status(404).send("Page not found");
 });
 
 export default router;
--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,20 +1,31 @@
-import userService from '../services/userService.js'; 
+import userService from "../services/userService.js";
 
 export const authMiddleware = async (req, res, next) => {
-  if (req.session.userId) {
-    try {
-      const loggedInUser = await userService.getUserById(req.session.userId);
-
-      if (loggedInUser) {
-        res.locals.loggedInUser = loggedInUser;
-        res.locals.isAdmin = loggedInUser.isAdmin;
-      }
-    } catch (err) {
-      console.error(
-        "Erreur lors de la récupération de l'utilisateur connecté :",
-        err
-      );
-    }
+  // Si l'utilisateur n'est pas authentifié, on le redirige vers login
+  if (!req.session.userId) {
+    return res.redirect("/login");
   }
-  next();
+
+  try {
+    // Cherche l'utilisateur connecté
+    const loggedInUser = await userService.getUserById(req.session.userId);
+
+    if (loggedInUser) {
+      // On enregistre les détails de l'utilisateur dans res.locals
+      res.locals.loggedInUser = loggedInUser;
+      res.locals.isAdmin = loggedInUser.isAdmin;
+      // Passe au middleware suivant ou au routeur
+      return next();
+    } else {
+      // Si l'utilisateur n'est pas trouvé, on le redirige vers login
+      return res.redirect("/login");
+    }
+  } catch (err) {
+    console.error(
+      "Erreur lors de la récupération de l'utilisateur connecté :",
+      err
+    );
+    // En cas d'erreur, redirige vers login
+    return res.redirect("/login");
+  }
 };
